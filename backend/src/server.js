df9185a6f89f198ad1f306c537ffcf0629e13702
yvtg <vyt0888@gmail.com>
2025-03-18 19:38:36 +0700

    Init backend directory structure

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req,res) => {
+    res.send("test")
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+});
